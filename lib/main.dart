c42596f16652143a5a35169100b35fd73d9a0c63
DavidArcalianu <arcalianu.david@gmail.com>
2022-06-06 15:15:58 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo' ,
       theme: ThemeData(primarySwatch: Colors.lightGreen,),
       home: const MyHomePage(title: 'Frigider virtual'),
     );
