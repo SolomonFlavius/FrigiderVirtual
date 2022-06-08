1694323e24825eb94fd51174abd51bd9d86286ee
DavidArcalianu <arcalianu.david@gmail.com>
2022-06-08 11:45:06 +0300

    08.06.2022 #2

---
+++ android/app/src/main/kotlin/com/example/frigider_virtual/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.frigider_virtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/pages/MarketPage.dart
+++ lib/pages/MarketPage.dart
@@ -3,5 +3,10 @@ import 'package:flutter/material.dart';
 class MarketPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+        title: Center(
+          child: Text('Market'),
+        )
+    ),
   );
 }
